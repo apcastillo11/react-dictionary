3e625483e0880b3dba93b60d624b3b2622354ade
a2dael2dawhy <55224339+apcastillo11@users.noreply.github.com>
2026-03-16 12:35:37 -0700

    Removed search button CSS page
    Replaced search button CSS page by adding styling to Phonetic.css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Results from "./Results";
 import Photos from "./Photos";
 import "./Dictionary.css";
-import "./SearchButton.css";
 import axios from "axios";
 
 export default function Dictionary(props) {
